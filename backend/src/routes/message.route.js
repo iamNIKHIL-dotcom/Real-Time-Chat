364f651606a09a765020ca3b1d11ad13560ca193
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-04-16 15:37:00 +0530

    msg routes completed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.get("/users", protectRoute,getUserForSideBar);
 
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
