7133947ab8b3c1a859cf250d919445a69947a7d2
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-04-16 12:47:58 +0530

    signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,58 @@
-export const signup = (req,res) =>{
-    
+import { generateToken } from "../lib/utils.js";
+import bcrypt from "bcryptjs"
+import User from "../models/user.model.js";
+
+
+export const signup = async (req,res) =>{
+    const { fullName, email, password } = req.body;
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({
+                msg : "password must length of 6"
+            })
+        }
+
+        const user = await User.findOne({email});
+        if(user){
+            return res.json({
+                msg : "user exists"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password : hashedPassword
+        })
+
+        if(newUser){
+            //generate token
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            })
+
+        }else{
+            res.status(400).json({
+                msg:"invalid data"
+            })
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });        
+    }
 }
 export const login  = (req,res) =>{
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT
 
 
