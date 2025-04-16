ee6c9cfbe1434d77ce0c13a66d4d08c842c3a60f
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-04-16 13:31:11 +0530

    middleware & update profilPic using cloudinary for image url

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,15 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
+
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
+
 const PORT = process.env.PORT
 
 
